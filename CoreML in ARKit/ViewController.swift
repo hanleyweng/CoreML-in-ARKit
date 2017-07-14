9b93976aba9a0c40d90f8aabbd45b4f4506bd777
Hanley Weng <hanleyweng@gmail.com>
2017-07-14 19:18:52 +1000

    ~ removed some unused ARSCNViewDelegate functions

--- CoreML in ARKit/ViewController.swift
+++ CoreML in ARKit/ViewController.swift
@@ -71,28 +71,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-    }
+    // ...
     
     // MARK: - CoreML Vision Handling
     
