e39867cfa613447d8e58bfecfd39768d5af035af
Damian Markowski <contact@damianmarkowski.pl>
2018-06-10 15:02:15 +0100

    large method split into smaller methods

--- CoreML in ARKit/ViewController.swift
+++ CoreML in ARKit/ViewController.swift
@@ -123,12 +123,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func createNewBubbleParentNode(_ text : String) -> SCNNode {
         // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
-        
-        // TEXT BILLBOARD CONSTRAINT
-        let billboardConstraint = SCNBillboardConstraint()
-        billboardConstraint.freeAxes = SCNBillboardAxis.Y
-        
-        // BUBBLE-TEXT
+        let bubbleNodeParent = SCNNode()
+        bubbleNodeParent.addChildNode(createBubbleNode(bubble: text))
+        bubbleNodeParent.addChildNode(createSphereNode())
+        bubbleNodeParent.constraints = [createBillboardConstraint()]
+        return bubbleNodeParent
+    }
+    
+    private func createBubbleNode(bubble: String) -> SCNNode {
+        let bubble = createBubble(text: bubble)
+        let (minBound, maxBound) = bubble.boundingBox
+        let bubbleNode = SCNNode(geometry: bubble)
+        // Centre Node - to Centre-Bottom point
+        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
+        // Reduce default text size
+        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        return bubbleNode
+    }
+    
+    private func createBubble(text: String) -> SCNText {
         let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
         var font = UIFont(name: "Futura", size: 0.15)
         font = font?.withTraits(traits: .traitBold)
@@ -139,27 +152,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         bubble.firstMaterial?.isDoubleSided = true
         // bubble.flatness // setting this too low can cause crashes.
         bubble.chamferRadius = CGFloat(bubbleDepth)
-        
-        // BUBBLE NODE
-        let (minBound, maxBound) = bubble.boundingBox
-        let bubbleNode = SCNNode(geometry: bubble)
-        // Centre Node - to Centre-Bottom point
-        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
-        // Reduce default text size
-        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
-        
-        // CENTRE POINT NODE
+        return bubble
+    }
+    
+    private func createSphereNode() -> SCNNode {
         let sphere = SCNSphere(radius: 0.005)
         sphere.firstMaterial?.diffuse.contents = UIColor.cyan
-        let sphereNode = SCNNode(geometry: sphere)
-        
-        // BUBBLE PARENT NODE
-        let bubbleNodeParent = SCNNode()
-        bubbleNodeParent.addChildNode(bubbleNode)
-        bubbleNodeParent.addChildNode(sphereNode)
-        bubbleNodeParent.constraints = [billboardConstraint]
-        
-        return bubbleNodeParent
+        return SCNNode(geometry: sphere)
+    }
+    
+    private func createBillboardConstraint() -> SCNBillboardConstraint {
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        return billboardConstraint
     }
     
     // MARK: - CoreML Vision Handling
