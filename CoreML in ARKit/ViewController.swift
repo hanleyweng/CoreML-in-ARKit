c1e4e35fdb65a003066daedeb144de553b082afe
Interstellar2Anubhav <dennisthecelebutante@gmail.com>
2021-08-23 21:46:19 +0530

    Fixed the on tap keyboard pop up issue and UI for iPhone X and above

--- CoreML in ARKit/ViewController.swift
+++ CoreML in ARKit/ViewController.swift
@@ -50,7 +50,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //////////////////////////////////////////////////
         
         // Set up Vision Model
-        guard let selectedModel = try? VNCoreMLModel(for: Inceptionv3().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
+        guard let selectedModel = try? VNCoreMLModel(for: MobileNet().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
             fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/ . Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
         }
         
